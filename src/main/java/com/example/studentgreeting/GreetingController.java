afb12db5d48ffce4a8a75e65e8887c7cc0d3c616
jonathanvineet <cvineetjonathan@gmail.com>
2026-04-01 08:37:29 +0530

    demo

--- src/main/java/com/example/studentgreeting/GreetingController.java
+++ src/main/java/com/example/studentgreeting/GreetingController.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
   @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
-  public String greeting() { return "<html><body><h1>Hello, Student!</h1></body></html>"; }
+  public String greeting() { return "<html><body><h1>Hello, System!</h1></body></html>"; }
 }
